3dd380e3aa82316dd69445eb276dd4d53a2f3bd6
Bj'z <zbjarchi@gmail.com>
2021-10-15 15:01:36 +0100

    useFetch

--- src/Compteur.js
+++ src/Compteur.js
@@ -10,6 +10,20 @@ function useIncrement(initialValue = 0, step = 1) {
   return [count, increment];
 }
 
+function useAutoIncrement(initialValue = 0, step = 1) {
+  const [count, increment] = useIncrement(initialValue, step);
+  useEffect(function () {
+    const timer = window.setInterval(function () {
+      increment();
+    }, 1000);
+
+    return function () {
+      clearInterval(timer);
+    };
+  }, []);
+  return count;
+}
+
 function useToggle(initialValue = true) {
   const [value, setValue] = useState(initialValue);
   const toggle = function () {
@@ -18,26 +32,102 @@ function useToggle(initialValue = true) {
   return [value, toggle];
 }
 
-function useAutoIncrement(initialValue = 0, step = 1) {
-  const [count, increment] = useIncrement(initialValue, step);
+function useFetch(url) {
+  const [state, setState] = useState({
+    items: [],
+    loading: true,
+  });
 
   useEffect(function () {
-    const timer = window.setInterval(function () {
-      increment()
-    }, 1000);
-
-    return function () {
-      clearInterval(timer);
-    };
+    (async function () {
+      const response = await fetch(url);
+      const responseData = await response.json();
+      if (response.ok) {
+        setState({
+          items: responseData,
+          loading: false,
+        });
+      } else {
+        alert(JSON.stringify(responseData));
+        // setState((s) => ({ ...s, loading: false }));
+      }
+    })();
   }, []);
 
-  return count;
+  return [state.loading, state.items];
 }
 
 function Compteur() {
-  const count = useAutoIncrement(0, 10);
+  const count = useAutoIncrement();
 
-  return <button>Increment {count}</button>;
+  return <button>Incrémenter {count}</button>;
+}
+
+function App() {
+  const [compteurVisible, toggleCompteur] = useToggle(true);
+
+  return (
+    <div>
+      Afficher le compteur
+      <input
+        type="checkbox"
+        onChange={toggleCompteur}
+        checked={compteurVisible}
+      />
+      <br />
+      {compteurVisible && <Compteur />}
+      <TodoList />
+      <PostTable />
+    </div>
+  );
+}
+
+function PostTable() {
+  const [loading, items] = useFetch(
+    "https://jsonplaceholder.typicode.com/comments?_limit=10"
+  );
+
+  if (loading) {
+    return "Chargement...";
+  }
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Email</th>
+          <th>Contenu</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.body}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function TodoList() {
+  const [loading, todos] = useFetch(
+    "https://jsonplaceholder.typicode.com/todos?_limit=10"
+  );
+
+  if (loading) {
+    return "Chargement...";
+  }
+
+  return (
+    <ul>
+      {todos.map((t) => (
+        <li key={t.id}>{t.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 function App2() {
@@ -52,6 +142,8 @@ function App2() {
       />
       <br />
       {compteurVisible && <Compteur />}
+      <TodoList />
+      <PostTable />
     </div>
   );
 }
